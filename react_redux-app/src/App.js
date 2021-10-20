665b737c5648006b67bf6f54b229000895a41c20
sergeycin <67867645+sergeycin@users.noreply.github.com>
2021-10-20 16:20:22 +0300

    component

--- react_redux-app/src/App.js
+++ react_redux-app/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import './App.css';
 function App() {
   return (
     <div className="App">
-     <div className="row">
-    
+     <div className="container">
+       <div className="col">
+       <p>FORM</p>
+       </div>
+       <div className="col">
+       <p>POSTS</p>
+       </div>
+       <div className="col">
+       <p>FETCHED POSTS</p>
+       </div>
     </div>
     </div>
   );
