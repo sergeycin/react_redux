11038585108d2240f5a0eca9d75f4efd4d19c3d5
sergeycin <67867645+sergeycin@users.noreply.github.com>
2021-10-26 22:12:07 +0300

    commit

--- react_redux-app/src/index.js
+++ react_redux-app/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {} from 'react-redux';
+import {Provider} from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(rootReducer)
-ReactDOM.render(
-  <React.StrictMode>
+
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>
+)
+render(app ,  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
